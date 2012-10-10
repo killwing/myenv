30e5f73595aa1bf6b635e17957388d8d9caa7176
kelvin <kowochen@gmail.com>
2012-10-10 13:00:46 +0800

    don't force ln

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 
 repo=$(cd "$(dirname "$0")"; pwd)
 
-ln -sf $repo/gitconfig $HOME/.gitconfig
-ln -sf $repo/bash_profile $HOME/.bash_profile
-ln -sf $repo/bashrc $HOME/.bashrc
-ln -sf $repo/inputrc $HOME/.inputrc
-ln -sf $repo/vim/vimrc $HOME/.vimrc
-ln -sf $repo/vim/vim $HOME/.vim
-ln -sf $repo/jshintrc $HOME/.jshintrc
+ln -sv $repo/gitconfig $HOME/.gitconfig
+ln -sv $repo/bash_profile $HOME/.bash_profile
+ln -sv $repo/bashrc $HOME/.bashrc
+ln -sv $repo/inputrc $HOME/.inputrc
+ln -sv $repo/vim/vimrc $HOME/.vimrc
+ln -snv $repo/vim/vim $HOME/.vim
+ln -sv $repo/jshintrc $HOME/.jshintrc
