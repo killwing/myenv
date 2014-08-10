0801218c7ea7fbc21f00318725fb245f286d1d68
kelvin <kowochen@gmail.com>
2014-08-10 23:37:53 +0800

    add gdbinit and printers

--- install.sh
+++ install.sh
@@ -10,4 +10,5 @@ ln -sfv  $repo/inputrc      $HOME/.inputrc
 ln -sfv  $repo/vim/vimrc    $HOME/.vimrc
 ln -sfnv $repo/vim/vim      $HOME/.vim
 ln -sfv  $repo/jshintrc     $HOME/.jshintrc
+ln -sfv  $repo/gdbinit      $HOME/.gdbinit
 ln -sfv  $repo/agignore     $HOME/.agignore
