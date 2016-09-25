15e2812a94575d19f6902e82477078180fd32cc2
kelvin <kowochen@gmail.com>
2016-09-26 00:51:08 +0800

    upgrade vim-go

--- install_go.sh
+++ install_go.sh
@@ -4,13 +4,18 @@ cmd="go get -u -v"
 packages=(
     # dev tools
     "golang.org/x/tools/cmd/goimports"
-    "golang.org/x/tools/cmd/oracle"
     "golang.org/x/tools/cmd/gorename"
+    "golang.org/x/tools/cmd/guru"
     "github.com/nsf/gocode"
     "github.com/rogpeppe/godef"
     "github.com/golang/lint/golint"
     "github.com/kisielk/errcheck"
     "github.com/jstemmer/gotags"
+    "github.com/alecthomas/gometalinter"
+    "github.com/klauspost/asmfmt/cmd/asmfmt"
+    "github.com/fatih/motion"
+    "github.com/zmb3/gogetdoc"
+    "github.com/josharian/impl"
     # system tools
     "github.com/svent/sift"
     # debug libraries
